00be8cfe94c1550a6e95b545bb835a4483f6c268
wbwdxh <wbw121124@126.com>
2026-09-25 12:28:01 +0800

    PWA 离线化：manifest + Service Worker + theme-color

---
+++ public/sw.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'wbwencrypt-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './assets/',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request).catch(() => caches.match('./index.html'))
+    );
+  } else {
+    event.respondWith(
+      caches.match(event.request).then((response) => response || fetch(event.request))
+    );
+  }
+});
